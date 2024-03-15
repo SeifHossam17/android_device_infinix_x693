33ae6760401d06135c29d61977c23d446be02d2d
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2024-03-15 09:48:46 +0200

    Update and rename omni_Infinix-X693.mk to twrp_Infinix-X693.mk

--- twrp_Infinix-X693.mk
+++ twrp_Infinix-X693.mk
@@ -5,18 +5,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+LOCAL_PATH := device/infinix/Infinix-X693
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Infinix-X693 device
 $(call inherit-product, device/infinix/Infinix-X693/device.mk)
 
 PRODUCT_DEVICE := Infinix-X693
-PRODUCT_NAME := omni_Infinix-X693
+PRODUCT_NAME := twrp_Infinix-X693
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X693
 PRODUCT_MANUFACTURER := infinix
